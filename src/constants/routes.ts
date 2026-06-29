8ae425ed7e8c828d86749e8989455cdf4cd409b5
alien_aura <divineazubuikeezinwa@gmail.com>
2026-06-29 13:09:51 +0100

    chore: initialize Next.js frontend application

---
+++ src/constants/routes.ts
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  LOGIN: "/auth/login",
+
+  ONBOARDING: "/auth/onboarding",
+
+  DASHBOARD: "/dashboard",
+
+  STUDENTS: "/dashboard/students",
+
+  TEACHERS: "/dashboard/teachers",
+
+  ATTENDANCE: "/dashboard/attendance",
+
+  SETTINGS: "/dashboard/settings",
+};
